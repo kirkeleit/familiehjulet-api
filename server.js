ffefef03ca048ec8c35a130aa4bf522a4ba3555f
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2023-02-12 22:20:51 +0100

    test

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const express = require ('express')
+require('dotenv').config()
+
+const express = require('express')
+const mysql = require('mysql2')
+const app = express()
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS})
+
+db.connect(function (err) {
+    if (err) console.error(err)
+    console.log("Connected to Database")
+})
+
+app.use(express.json())
+
+const registrationRouter = require('./routes/registration')
+app.use('/registration', registrationRouter)
+
+app.listen(3000, () => console.log('Server Started'))

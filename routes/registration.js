0d37af06c3a571639d67fdeeabc2335cbe02bcf4
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2023-02-12 22:26:48 +0100

    Update registration.js

--- routes/registration.js
+++ routes/registration.js
@@ -1,4 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+router.post('/:id', (req, res) => {
+    res.send('Hello World')
+})
+
 module.exports = router

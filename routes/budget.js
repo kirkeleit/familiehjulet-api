491af8141b87e79ea30df5d1a9807956496ee23f
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2023-02-22 20:19:55 +0100

    Update budget.js

--- routes/budget.js
+++ routes/budget.js
@@ -2,20 +2,42 @@ const express = require('express')
 const router = express.Router()
 const crypto = require('crypto')
 
+
 const dbPool = require('../database')
 const TokenAuthorization = require("../authorization")
 
-router.post('/', TokenAuthorization, async (req, res) => {
+
+
+router.post('/expenses', TokenAuthorization, async (req, res) => {
     console.log ("  New expense: "+req.body.Description+" / "+req.body.Cost)
     var ExpenseID = crypto.randomUUID();
-    UserID = "test"
+    UserID = req.user.UserID
     console.log ("  Expense ID: "+ExpenseID)
     var $sql = "INSERT INTO Budget_Expenses (ExpenseID,UserID,Description,Cost) VALUES ('"+ExpenseID+"','"+UserID+"','"+req.body.Description+"','"+req.body.Cost+"')"
     dbPool.query($sql, function (err, result) {
       if (err) throw err;
       console.log("Added budget expense to database.");
     });
-    res.status(201).json({ "ExpenseID": ExpenseID})
+    res.status(201).json({ "ExpenseID": ExpenseID, "Description": req.body.Description, "Cost": req.body.Cost})
 })
+
+router.get('/expenses', TokenAuthorization, (req, res) => {
+    console.log("  Expenses requested for UserID: "+req.user.UserID)
+    var $sql = "SELECT ExpenseID,Description,Cost FROM Budget_Expenses WHERE (UserID='"+req.user.UserID+"')"
+    try {
+      dbPool.query($sql, function (err, resExpenses, fields) {
+        if (err) {
+          console.log(err);
+          return (res.status(500).json())
+        }
+        console.log(resExpenses);
+        res.status(200).json({resExpenses})
+      });
+    }
+    catch(err) {
+      console.log(err);
+      //res.status(400).json();
+    }
+  })
 
 module.exports = router

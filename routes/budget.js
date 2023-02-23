bdceaff4dc42c558ea8ed63ece09bf2520a18e76
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2023-02-23 19:05:14 +0100

    Update budget.js

--- routes/budget.js
+++ routes/budget.js
@@ -7,7 +7,6 @@ const dbPool = require('../database')
 const TokenAuthorization = require("../authorization")
 
 
-
 router.post('/expenses', TokenAuthorization, async (req, res) => {
     console.log ("  New expense: "+req.body.Description+" / "+req.body.Cost)
     var ExpenseID = crypto.randomUUID();
@@ -38,6 +37,41 @@ router.get('/expenses', TokenAuthorization, (req, res) => {
       console.log(err);
       //res.status(400).json();
     }
+})
+
+router.delete('/expenses', TokenAuthorization, (req, res) => {
+    const ExpenseID = req.body.ExpenseID
+    console.log("  Deleting budget expense with ExpenseID: "+ExpenseID)
+    
+    var sql = "DELETE FROM Budget_Expenses WHERE ExpenseID='"+ExpenseID+"' LIMIT 1"
+    console.log(sql)
+    dbPool.query(sql, function (err, result, fields) {
+      if (err) {
+        console.log(err);
+        return (res.status(500).json())
+      }
+      console.log(result);
+      res.status(200).json({result})
+    });
   })
+
+router.get('/categories', TokenAuthorization, (req, res) => {
+  console.log("  Budget categories requested.")
+  var $sql = "SELECT CategoryID,Name,Description FROM Budget_Categories WHERE (Global=1)"
+  try {
+    dbPool.query($sql, function (err, resCategories, fields) {
+      if (err) {
+        console.log(err);
+        return (res.status(500).json())
+      }
+      console.log(resCategories);
+      res.status(200).json({resCategories})
+    });
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).json();
+  }
+})
 
 module.exports = router
